e935bd7d005ff9332184e3479069fb60d0adf749
GabrielGondim98 <gabrielvidalgondim@gmail.com>
2020-11-03 18:28:27 -0300

    setting actions and reducers

---
+++ src/actions/userAction.js
@@ -0,0 +1,9 @@
+const setUser = (userObj) => {
+  return { type: "SET_USER", payload: userObj };
+};
+
+const logOut = () => {
+  return { type: "LOG_OUT" };
+};
+
+export default { setUser, logOut };
